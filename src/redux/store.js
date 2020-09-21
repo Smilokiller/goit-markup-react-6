e90b9cd6d429abfcd0c5ae4da6e70e16b4d37905
Tofik <smilokiller@mail.ru>
2020-09-21 15:55:46 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,34 @@
 import { telBookReducers } from "./telBookReducers";
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+// import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["filter"],
 };
 
 const { reducer } = telBookReducers;
 const persistedReducer = persistReducer(persistConfig, reducer);
-const store = configureStore({ reducer: persistedReducer });
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
 persistStore(store);
 export default store;
